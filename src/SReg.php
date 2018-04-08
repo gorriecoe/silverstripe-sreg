4bdd32d79b5453fec32bc3d8e21f865924adfb6c
Gorrie Coe <gorriecoe@gmail.com>
2018-04-08 12:26:15 +1200

    Added the ability to parse strings without {$}.   sreg can now be used in a similar way to relField with the added bonus of using | to create fallback chains.

--- src/SReg.php
+++ src/SReg.php
@@ -10,7 +10,7 @@ use SilverStripe\ORM\DataExtension;
 class SReg extends DataExtension
 {
     /**
-     * Simple syntax parser that allows you to use .ss template like variables in a string
+     * Simple syntax tokenizer that allows you to use .ss template like variables in a string
      * e.g 'Lorem ipsum {$Relation.Title}'
      * @param string $string
      * @return string|null
@@ -18,30 +18,46 @@ class SReg extends DataExtension
     public function sreg($string)
     {
         $owner = $this->owner;
+        $count = 0;
         $parsedString = preg_replace_callback(
             '/\{\$([\w\d\s.|]*)\}/',
-            function ($matches) use($owner)
+            function ($matches)
             {
-                $values = explode('|', $matches[1]);
-                $count = count($values);
-                foreach ($values as $key => $value) {
-                    if ($relValue = $owner->relField($value)) {
-                        return $relValue;
-                    } elseif (
-                        $value != 'null' &&
-                        $count > 1 &&
-                        $key + 1 == $count &&
-                        $owner->hasMethod($value)
-                    ) {
-                        return $value;
-                    }
-                }
-
+                return $this->sregValue($matches[1]);
             },
-            $string
+            $string,
+            -1,
+            $count
         );
+        if (!$count && !preg_match('/ /', $string)) {
+            $parsedString = $this->sregValue($string);
+        }
         if (trim($parsedString) != '') {
             return $parsedString;
+        }
+    }
+
+    /**
+     * Splits string by |, loops each segment and returns the first non-null value
+     * @param string $string
+     * @return string|null
+     */
+    private function sregValue($value='')
+    {
+        $owner = $this->owner;
+        $values = explode('|', $value);
+        $count = count($values);
+        foreach ($values as $key => $value) {
+            if ($relValue = $owner->relField($value)) {
+                return $relValue;
+            } elseif (
+                $value != 'null' &&
+                $count > 1 &&
+                $key + 1 == $count &&
+                !$owner->hasMethod($value)
+            ) {
+                return $value;
+            }
         }
     }
 }
