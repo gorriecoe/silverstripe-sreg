2693e653b4cbd3624a20294be5ab391f0ac5067d
Gorrie Coe <gorriecoe@gmail.com>
2018-05-03 09:43:51 +1200

    Added custom field traverse

--- src/SReg.php
+++ src/SReg.php
@@ -3,6 +3,7 @@
 namespace gorriecoe\sreg;
 
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\Core\ClassInfo;
 
 /**
  * @package silverstripe-sreg
@@ -46,10 +47,10 @@ class SReg extends DataExtension
         $owner = $this->owner;
         $values = explode('|', $value);
         $count = count($values);
-        foreach ($values as $key => $value) {
-            if ($value == '' || $value == 'null') {
+        foreach ($values as $key => $fieldPath) {
+            if ($fieldPath == '' || $fieldPath == 'null') {
                 return null;
-            } elseif ($relValue = $owner->relField($value)) {
+            } elseif ($relValue = $this->sregTraverse($fieldPath)) {
                 if (is_object($relValue) && method_exists($relValue, 'AbsoluteLink')) {
                     if ($link = $relValue->AbsoluteLink()) {
                         return $link;
@@ -59,9 +60,56 @@ class SReg extends DataExtension
                 } else {
                     return $relValue;
                 }
-            } elseif ($key + 1 == $count && !$owner->hasMethod($value) ) {
-                return $value;
+            } elseif ($key + 1 == $count && !$owner->hasMethod($fieldPath) ) {
+                return $fieldPath;
             }
         }
+    }
+
+    private function sregTraverse($fieldPath)
+    {
+        $owner = $this->owner;
+        $component = $owner;
+        // Parse all relations
+        foreach (explode('.', $fieldPath) as $relation) {
+            if (!$component) {
+                return null;
+            }
+
+            preg_match('/([\w\d]*)(\(([\w\d\s,\'"]*)\))?/', $relation, $matches);
+
+            if (isset($matches[1])) {
+                $relation = $matches[1];
+            }
+            // Inspect relation type
+            if (ClassInfo::hasMethod($component, $relation)) {
+                if (isset($matches[3])) {
+                    $component = call_user_func_array(
+                        [$component, $relation],
+                        explode(',', $matches[3])
+                    );
+                } else {
+                    $component = $component->{$relation}();
+                }
+            } elseif (
+                $component instanceof Relation ||
+                $component instanceof DataList
+            ) {
+                // $relation could either be a field (aggregate), or another  drelation
+                $singleton = DataObject::singleton($component->dataClass());
+                $component = $singleton->dbObject($relation) ?: $component->relation($relation);
+            } elseif (
+                $component instanceof DataObject &&
+                ($dbObject = $component->dbObject($relation))
+            ) {
+                $component = $dbObject;
+            } else {
+                $component = $component->obj($relation);
+                if (ClassInfo::hasMethod($component, 'Plain')) {
+                    $component = $component->Plain();
+                }
+            }
+        }
+        return $component;
     }
 }
